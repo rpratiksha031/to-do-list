62474b886b3bf1792d0183f6a8c31a44ebf036b8
pratiksharawat <rpratiksha031@gmail.com>
2024-09-04 02:13:58 +0530

    res-3

--- src/ui/Body.jsx
+++ src/ui/Body.jsx
@@ -31,13 +31,13 @@ function Body() {
   return (
     <div className="bg-bg-image  py-40 text-center text-2xl  sm:py-4 xs:py-20 xs:font-medium">
       <div className="bg-teal-500 py-40 mx-64 mb-5 mx-60  sm:mx-20 sm:my-5 xs:grid-rows-4 xs:mx-8 xs:my-0 xs:py-8">
-        <div className="font-bold font-serif mb-10 xs:font-medium xs:mb-5">
+        <div className="font-bold font-serif mb-10 xs:font-medium xs:mb-5 xs:py-5">
           ENTER YOUR TASK
         </div>
         <input
           type="text"
           placeholder="add-tasks"
-          className="mr-5 px-5 py-3 sm:mr-1 sm:px-0 sm:py-0 xs:mr-0 xs:py-0 xs:px-0"
+          className="mr-5 px-5 py-3 sm:mr-1 sm:px-0 sm:py-0 xs:mr-0 xs:py-0 xs:px-0 xs:mx-3 xs:mx-0 xs:mb-3"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
@@ -56,14 +56,17 @@ function Body() {
         {/* // tasksss */}
         <div>
           {list.map((ele, index) => (
-            <div key={index} className="bg-teal-300 mx-10 mt-6 py-3 space-y-10">
+            <div
+              key={index}
+              className="bg-teal-300 mx-10 mt-6 py-3 space-y-10 xs:py-0 "
+            >
               <div>
                 <span className="font-bold text-2xl">{ele.task}</span>
-                <span className="ml-40 font-serif font-bold text-2xl">
+                <span className="ml-40 font-serif font-bold text-2xl xs:ml-0">
                   {ele.time}
                 </span>
                 <button
-                  className="ml-40 text-2xl "
+                  className="ml-40 text-2xl xs:ml-20"
                   onClick={() => handleDelete(index)}
                 >
                   <RiDeleteBinLine />
@@ -74,7 +77,7 @@ function Body() {
         </div>
         {list.length >= 1 && (
           <button
-            className="bg-teal-100 mt-10 px-4 py-2 hover:bg-stone-200"
+            className="bg-teal-100 mt-10 px-4 py-2 hover:bg-stone-200 xs:py-1"
             onClick={handleDeleteAll}
           >
             Clear All
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="bg-teal-500 text-center py-8 text-2xl font-serif font-bold xs:font-medium">
+    <div className="bg-teal-500 text-center py-8 text-2xl font-serif font-bold xs:font-medium xs:py-10">
       TIME IS MONEY
     </div>
   );
